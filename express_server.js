d13869d31d99547ea9879515cd150be69cb69ac7
“mehmetkah” <mehmetkahca@gmail.com>
2022-09-08 22:15:07 -0600

    testingnewRoute

--- express_server.js
+++ express_server.js
@@ -21,3 +21,11 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: "http://www.lighthouselabs.ca" };
+  res.render("urls_show", templateVars);
+});
