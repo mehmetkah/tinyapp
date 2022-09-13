369933e70cebf5ef6c3c7f5ba25af7792e357694
“mehmetkah” <mehmetkahca@gmail.com>
2022-09-12 19:24:18 -0600

    URL shortening part 2

--- express_server.js
+++ express_server.js
@@ -5,22 +5,20 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
- function generateRandomString() {
-let result = ""
-const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-for (let i = 0; i < 6; i++) {
-  result += characters.charAt(Math.random()* characters.length)
-  
-    
+
+function generateRandomString() {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.random() * characters.length);
   }
-  return result
-} ;
+  return result;
+}
 //console.log(generateRandomString())
-
-
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -35,18 +33,33 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
+
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: "http://www.lighthouselabs.ca" };
+  const templateVars = {
+    id: req.params.id,
+    longURL: "http://www.lighthouselabs.ca",
+  };
+
   res.render("urls_show", templateVars);
 });
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id]
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const id = generateRandomString();
+  const longURL = req.body.longURL;
+  urlDatabase[id] = longURL;
+  res.redirect(`/urls/${id}`); // Respond with 'Ok' (we will replace this)
 });
